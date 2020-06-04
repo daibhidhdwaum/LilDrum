fa471b7a6e872572e070a4574451409850c85338
David Downie <daviddownie@gmail.com>
2020-06-04 14:44:05 -0400

    Built initail data structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,31 @@
-$(() => {
+const lilDrum = {};
 
+// create array of paths to sound sources
+lilDrum.instruments = [
+    {
+        soundPath: `./assets/sounds/kick.wav`
+    },
+    {
+        soundPath: `./assets/sounds/snare.wav`
+    },
+    {
+        soundPath: `./assets/sounds/closedHat.wav`
+    },
+    {
+        soundPath: `./assets/sounds/openHat.wav`
+    }
+]
+// initialise audio connction
+// created functionto play sound
+    // assign source of sound to be played
+    // play sourc
+
+// grab sound requested
+    // callback to trigger when sound is played
+
+    lilDrum.init = () => {
+    }
+
+$(() => {
+    lilDrim.init();
 })
